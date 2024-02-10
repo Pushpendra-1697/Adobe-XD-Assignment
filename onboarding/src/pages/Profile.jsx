a8b4ab2d5431c9a8b66320fed3258e186896138e
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2024-02-10 21:46:57 +0530

    responsive profile page

--- onboarding/src/pages/Profile.jsx
+++ onboarding/src/pages/Profile.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      <h1>Account Settings</h1>
+      <div>
+        <div>
+          <div>
+            <img src={'./profile.png'} alt='Profile' />
+            <img id='camara' src='./camara.svg' alt='camara' />
+          </div>
+          <div>
+            <h4>Marry Doe</h4>
+            <p>Marry@Gmail.Com</p>
+          </div>
+        </div>
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore nam in perspiciatis reprehenderit quam impedit labore
+        </div>
+      </div>
+    </div>
   );
 }
 
