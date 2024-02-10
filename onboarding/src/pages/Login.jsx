3e46e190940866463c198ec95ce3c3669c66b218
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2024-02-10 19:44:19 +0530

    added signup page

--- onboarding/src/pages/Login.jsx
+++ onboarding/src/pages/Login.jsx
@@ -11,11 +11,11 @@ const Login = () => {
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
       <div>
         <div className='form-group'>
-          <label>Email Address</label>
+          <label htmlFor='email'>Email Address</label>
           <input type='email' placeholder='Enter email address' value={email} name='email' onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className='form-group'>
-          <label>Password</label>
+          <label htmlFor='password'>Password</label>
           <input type='password' placeholder='Enter password' value={password} name='password' onChange={(e) => setPassword(e.target.value)} />
         </div>
         <Button label={'Login'} bgColor={(email && password) ? '#6C25FF' : '#CBCBCB'} color={'#ECECEC'} isDisabled={(email && password) ? false : true} redirectPath='/profile' />
--- onboarding/src/pages/Signup.jsx
+++ onboarding/src/pages/Signup.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../components/Button';
 
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [companyName, setCompanyName] = useState('');
+  const [agency, setAgency] = useState('');
+
+
   return (
     <div className='signup'>
-      <h1>Create your PopX account</h1>
-      
+      <h1 style={{ marginBottom: '10px' }}>Create your PopX account</h1>
+      <div>
+        <div className='form-group'>
+          <label htmlFor='full_name'>Full Name<span>*</span></label>
+          <input required type='text' placeholder='Enter name' value={name} name='name' onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone_number'>Phone number<span>*</span></label>
+          <input required type='tel' placeholder='Enter Phone number' value={phone} name='phone' onChange={(e) => setPhone(e.target.value)} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address<span>*</span></label>
+          <input required type='email' placeholder='Enter email address' value={email} name='email' onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password<span>*</span></label>
+          <input required type='password' placeholder='Enter password' value={password} name='password' onChange={(e) => setPassword(e.target.value)} />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='company_name'>Company name</label>
+          <input type='text' placeholder='Enter company name' value={companyName} name='company name' onChange={(e) => setCompanyName(e.target.value)} />
+        </div>
+        <div className='agency'>
+          <label htmlFor='agency'>Are you an Agency?<span>*</span></label>
+          <div>
+            <div>
+              <input required type='radio' value={'agency'} name='agency' onChange={(e) => setAgency(e.target.value)} />
+              <label htmlFor='yes'>Yes</label>
+            </div>
+            <div>
+              <input required type='radio' value={'agency'} name='agency' onChange={(e) => setAgency(e.target.value)} />
+              <label htmlFor='no'>No</label>
+            </div>
+          </div>
+        </div>
+        <Button label={'Create Account'} bgColor={(email && password && name && phone && agency) ? '#6C25FF' : '#CBCBCB'} color={'#ECECEC'} isDisabled={(email && password && name && phone && agency) ? false : true} redirectPath='/profile' />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
